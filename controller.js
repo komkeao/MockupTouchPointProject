fc738c7a469d41620039b8c2ad7138ff5dd1c144
Komkeao <kcsthai1995@gmail.com>
2017-06-23 17:29:20 +0700

    fixed all component

--- controller.js
+++ controller.js
@@ -1,48 +1,47 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(["$routeProvider",function ($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl : "index.html"
+            templateUrl: "index.html"
         })
         .when("/formInfo", {
-            templateUrl : "formInfo.html"
+            templateUrl: "formInfo.html"
         })
-         .when("/summary", {
-            templateUrl : "summary.html"
+        .when("/summary", {
+            templateUrl: "summary.html"
         })
-          .when("/register", {
-            templateUrl : "register.html"
+        .when("/register", {
+            templateUrl: "register.html"
         })
-           .when("/purpose", {
-            templateUrl : "purpose.html"
+        .when("/purpose", {
+            templateUrl: "purpose.html"
         })
-});
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope,$window) {
-    $scope.goToFormInfo=function (purpose) {
-      //  $localStorage.purpose = purpose;
-        $window.location="/formInfo";
+}]);
+app.controller('myCtrl',["$scope","$window", function ($scope, $window) {
+    $scope.goToFormInfo = function (purpose) {
+        //  $localStorage.purpose = purpose;
+        $window.location = "/formInfo";
     }
 
-      $scope.goToSummary=function () {
-        $window.location="/summary";
+    $scope.goToSummary = function () {
+        $window.location = "/summary";
     }
 
-    $scope.goToPurpose=function () {
-        $window.location="/purpose";
+    $scope.goToPurpose = function () {
+        $window.location = "/purpose";
     }
 
-    $scope.goToRegis=function () {
-        $window.location="/register";
+    $scope.goToRegis = function () {
+        $window.location = "/register";
     }
-     $scope.goToIndex=function () {
-        $window.location="/";
+    $scope.goToIndex = function () {
+        $window.location = "/";
     }
 
-    $scope.close=function(){
+    $scope.close = function () {
         $window.close();
     }
 
-    $scope.back=function(){
+    $scope.back = function () {
     }
-});
+}]);
