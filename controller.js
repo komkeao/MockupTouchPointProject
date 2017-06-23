77589664ab937c43e0bd0260737df9e79c4697d4
papawarin <papawarinsrimeechai@gmail.com>
2017-06-23 15:13:18 +0700

    all view

--- controller.js
+++ controller.js
@@ -7,10 +7,42 @@ app.config(function($routeProvider) {
         .when("/formInfo", {
             templateUrl : "formInfo.html"
         })
+         .when("/summary", {
+            templateUrl : "summary.html"
+        })
+          .when("/register", {
+            templateUrl : "register.html"
+        })
+           .when("/purpose", {
+            templateUrl : "purpose.html"
+        })
 });
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope,$window) {
-    $scope.goToFormInfo=function () {
+    $scope.goToFormInfo=function (purpose) {
+      //  $localStorage.purpose = purpose;
         $window.location="/formInfo";
+    }
+
+      $scope.goToSummary=function () {
+        $window.location="/summary";
+    }
+
+    $scope.goToPurpose=function () {
+        $window.location="/purpose";
+    }
+
+    $scope.goToRegis=function () {
+        $window.location="/register";
+    }
+     $scope.goToIndex=function () {
+        $window.location="/";
+    }
+
+    $scope.close=function(){
+        $window.close();
+    }
+
+    $scope.back=function(){
     }
 });
