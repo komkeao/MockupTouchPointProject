15039f412743b06fc5df20c7554d48fd6e06151b
papawarin <papawarinsrimeechai@gmail.com>
2017-06-23 10:27:41 +0700

    commit first

---
+++ route.js
@@ -0,0 +1,12 @@
+<script>
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "index.html"
+    })
+    .when("/formInfo", {
+        templateUrl : "formInfo.html"
+    })
+});
+<script/>
