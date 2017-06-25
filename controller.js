1fba3b15533a59f2d54c932014734b4c391bfde0
Komkeao <kcsthai1995@gmail.com>
2017-06-25 23:42:26 +0700

    show result summary on view and change ng-model on formInfo

--- controller.js
+++ controller.js
@@ -19,15 +19,39 @@ app.config(["$routeProvider",function ($routeProvider) {
 }]);
 app.controller('myCtrl',["$scope","$window", function ($scope, $window) {
     $scope.goToFormInfo = function (purpose) {
-        //  $localStorage.purpose = purpose;
         $window.location = "/formInfo";
     }
-
+    $scope.init = function () {
+        if($window.localStorage.getItem("firstname")=="undefined"||$window.localStorage.getItem("lastname")==undefined||$window.localStorage.getItem("lastname")=="undefined"){
+            $scope.user ="นายแสงดาว สาวตะวัน";
+        }else {
+            $scope.user =$window.localStorage.getItem("firstname")+" "+$window.localStorage.getItem("lastname");
+        }
+        $scope.recover =$window.localStorage.getItem("recover")
+        $scope.time=$window.localStorage.getItem("timeRecover")
+        $scope.bath=$window.localStorage.getItem("timeRecover")/12
+        $scope.limit_recover =$window.localStorage.getItem("recover")
+        $scope.limit_time=$window.localStorage.getItem("timeRecover")
+        $scope.limit_bath=$window.localStorage.getItem("timeRecover")/12
+    };
     $scope.goToSummary = function () {
+        $window.localStorage.setItem("salary",$scope.salary);
+        $window.localStorage.setItem("payment",$scope.payment);
+        $window.localStorage.setItem("Debt",$scope.Debt);
+        $window.localStorage.setItem("recover",$scope.recover);
+        $window.localStorage.setItem("phone",$scope.phone);
+        $window.localStorage.setItem("timeRecover",$scope.timeRecover);
         $window.location = "/summary";
     }
 
     $scope.goToPurpose = function () {
+        $window.localStorage.setItem("firstname",$scope.firstname);
+        $window.localStorage.setItem("lastname",$scope.lastname);
+        $window.localStorage.setItem("age",$scope.age);
+        $window.localStorage.setItem("job",$scope.job);
+        $window.localStorage.setItem("phone",$scope.phone);
+        $window.localStorage.setItem("time",$scope.time);
+        $window.localStorage.setItem("email",$scope.email);
         $window.location = "/purpose";
     }
 
